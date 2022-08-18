5245063ad89abc552ebcc1a438c03e688e44b42e
Michelle Mabuyo <michelle.mabuyo@gmail.com>
2022-08-18 14:46:11 -0400

    feat: add auth logic in preparation for router passing auth headers

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql, AuthenticationError } = require('apollo-server');
 const { buildSubgraphSchema } = require('@apollo/subgraph');
 const { readFileSync } = require('fs');
+const axios = require('axios');
 
 const typeDefs = gql(readFileSync('./accounts.graphql', { encoding: 'utf-8' }));
 const resolvers = require('./resolvers');
+
 const AccountsAPI = require('./datasources/accounts');
 
 const server = new ApolloServer({
-  schema: buildSubgraphSchema([{ typeDefs, resolvers }]),
+  schema: buildSubgraphSchema({ typeDefs, resolvers }),
   dataSources: () => {
     return {
       accountsAPI: new AccountsAPI(),
     };
   },
-  context: ({ req }) => {
-    return { userId: req.headers.userid, userRole: req.headers.userrole };
+  context: async ({ req }) => {
+    if (req.headers.authorization) {
+      const token = req.headers.authorization || '';
+      const userId = token.split(' ')[1]; // get the user name after 'Bearer '
+      if (userId) {
+        const { data } = await axios
+          .get(`https://rt-airlock-services-account.herokuapp.com/login/${userId}`)
+          .catch((error) => {
+            throw new AuthenticationError(error.message);
+          });
+
+        return { userId: data.id, userRole: data.role };
+      }
+    } else {
+      return { userId: req.headers.userid, userRole: req.headers.userrole };
+    }
   },
 });
 
